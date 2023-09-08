ffb691d3bceb344d0fc9c40f84310f773371b32b
Haruo-dev <saintsaen18@gmail.com>
2023-09-08 18:54:41 -0500

    init

---
+++ assets/js/index.js
@@ -0,0 +1,326 @@
+ScrollReveal().reveal('.li', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 450,
+    origin : 'bottom',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.li-1', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 500,
+    origin : 'bottom',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.li-2', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 550,
+    origin : 'bottom',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.li-3', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    origin : 'bottom',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.nav-icon', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'left',
+    distance : '100px',
+});
+
+ScrollReveal().reveal('.tele', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'left',
+    distance : '100px',
+});
+
+ScrollReveal().reveal('.tele-2', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.direc', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'left',
+    distance : '100px',
+});
+
+ScrollReveal().reveal('.direc-2', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '100px',
+});
+
+ScrollReveal().reveal('.red-1', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'button',
+    distance : '100px',
+});
+
+ScrollReveal().reveal('.inst', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'right',
+    distance : '500px',
+});
+
+ScrollReveal().reveal('.pau', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 600,
+    duration: 1000,
+    origin : 'right',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.tha', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 650,
+    duration: 1000,
+    origin : 'right',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.dani', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 700,
+    duration: 1000,
+    origin : 'right',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.nico', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 750,
+    duration: 1000,
+    origin : 'right',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.luna', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 800,
+    duration: 1000,
+    origin : 'right',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.sara', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 800,
+    duration: 1000,
+    origin : 'right',
+    distance : '-100px',
+});
+
+ScrollReveal().reveal('.about-us ', {
+    easing: 'cubic-bezier(0.5, 0, 0, 1)',
+    delay: 400,
+    duration: 1000,
+    origin : 'top',
+    distance : '100px',
+});
+
+
+ScrollReveal().reveal('.empre-1 ', {
+    delay: 800,
+    duration: 1000,
+    origin : 'left',
+    distance : '300px',
+});
+
+setTimeout(function() {
+    ScrollReveal().reveal('.p', {
+        delay: 850,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+    
+    ScrollReveal().reveal('.resp', {
+        delay: 850,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+    ScrollReveal().reveal('.img', {
+        delay: 900,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.trab', {
+        delay: 800,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+    ScrollReveal().reveal('.img-2', {
+        delay: 850,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.trans', {
+        delay: 750,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+    ScrollReveal().reveal('.img-3', {
+        delay: 800,
+        duration: 900,
+        origin : 'left',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.img-4', {
+        delay: 1000,
+        duration: 1100,
+        origin : 'righ',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.p-1', {
+        delay: 950,
+        duration: 1000,
+        origin : 'righ',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.p-2', {
+        delay: 1300,
+        duration: 1000,
+        origin : 'left',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.p-3', {
+        delay: 1400,
+        duration: 1000,
+        origin : 'right',
+        distance : '300px',
+    });
+
+    ScrollReveal().reveal('.p-4', {
+        delay: 1500,
+        duration: 1000,
+        origin : 'right',
+        distance : '300px',
+    });
+}, 1000);
+
+ScrollReveal().reveal('.empre-2 ', {
+    delay: 1100,
+    duration: 1000,
+    origin : 'right',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.empre-3', {
+    delay: 1400,
+    duration: 1000,
+    origin : 'left',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.empre-4', {
+    delay: 1700,
+    duration: 1000,
+    origin : 'right',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.card-1', {
+    delay: 800,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.card_img-1', {
+    delay: 900,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.card-2', {
+    delay: 900,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.card_img-2', {
+    delay: 1000,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.card-3', {
+    delay: 1000,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '300px',
+});
+
+ScrollReveal().reveal('.card_img-3', {
+    delay: 1100,
+    duration: 1000,
+    origin : 'bottom',
+    distance : '300px',
+});
+
+
+
+function asignarClaseDespuesDeTiempo(componente, clase, tiempo) {
+    // Utiliza setTimeout para retrasar la asignación de la clase
+    setTimeout(function() {
+        // Agrega la clase al componente
+        componente.classList.add(clase);
+    }, tiempo);
+}
+
+function removerClase(componente, clase) {
+    // Utiliza setTimeout para retrasar la asignación de la clase
+    setTimeout(function() {
+        // Agrega la clase al componente
+        componente.classList.remove(clase);
+    }, 3000);
+}
+let tele = document.getElementById('tele');
+let direc = document.getElementById('direc');
+let h1 = document.getElementById('h1');
+// let img = document.getElementById('img');
+let p3 = document.getElementById('p-3');
+let p4 = document.getElementById('p-4');
+
+asignarClaseDespuesDeTiempo(tele, 'jello-horizontal', 2000);
+asignarClaseDespuesDeTiempo(direc, 'jello-horizontal', 2000);
+removerClase(h1,'h1');
+asignarClaseDespuesDeTiempo(h1, 'text-shadow-pop-bottom', 1500);
+asignarClaseDespuesDeTiempo(p3, 'heartbeat', 2000);
+asignarClaseDespuesDeTiempo(p4, 'heartbeat', 2500);
+// asignarClaseDespuesDeTiempo(img, 'slide-in-fwd-center', 3000);
+
